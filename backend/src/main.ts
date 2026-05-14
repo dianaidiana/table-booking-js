dca5fce3ea4d9b6114510ea86a3e1bc1465bb3a6
Diana Carbajal <di.fayolle@gmail.com>
2026-05-14 15:59:20 +0200

    add default error handler

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { initDb, closeDb } from "./db-setup.js";
 import { adminRoutes } from "./routes/admin/admin-routes.js";
+import z from "zod";
 
 const app = express();
 const port = 8080;
@@ -9,6 +10,28 @@ initDb();
 
 app.use(express.json());
 app.use("/admin", adminRoutes);
+
+app.use(
+    (
+        err: Error,
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
+    ) => {
+        if (res.headersSent) {
+            return next(err);
+        }
+
+        if (err instanceof z.ZodError) {
+            return res
+                .status(400)
+                .json({ error: "Invalid input", issues: err.issues });
+        }
+
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    },
+);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
