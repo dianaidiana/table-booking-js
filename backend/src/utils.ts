7076a23c2d58adda713cad66cccb61ad2043050d
Diana Carbajal <di.fayolle@gmail.com>
2026-07-05 20:53:12 +0200

    add getTimeFromMinutes function to utils

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,3 +1,8 @@
 export function getMinutesFrom00hs(time: Temporal.PlainTime) {
     return time.hour * 60 + time.minute;
 }
+
+export function getTimeFromMinutes(totalMinutes: number): Temporal.PlainTime {
+    const midnight = new Temporal.PlainTime(0, 0);
+    return midnight.add({ minutes: totalMinutes });
+}
