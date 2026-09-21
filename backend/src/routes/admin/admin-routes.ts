eab780f7f2abb0778ecb24e16214eb1344869a5c
Diana Carbajal <di.fayolle@gmail.com>
2026-09-21 15:26:59 +0200

    add bookingsRoutes to admin

--- backend/src/routes/admin/admin-routes.ts
+++ backend/src/routes/admin/admin-routes.ts
@@ -3,6 +3,7 @@ import { settingsRoutes } from "../../core/settings/settings.routes.ts";
 import { tableGroupsRoutes } from "../../core/table-groups/table-groups.routes.ts";
 import { tablesRoutes } from "../../core/tables/tables.routes.ts";
 import { openingHoursRoutes } from "../../core/opening-hours/opening-hours.routes.ts";
+import { bookingsRoutes } from "../../core/bookings/bookings.routes.ts";
 
 export const adminRoutes = Router();
 
@@ -10,3 +11,4 @@ adminRoutes.use("/settings", settingsRoutes);
 adminRoutes.use("/table-groups", tableGroupsRoutes);
 adminRoutes.use("/tables", tablesRoutes);
 adminRoutes.use("/opening-hours", openingHoursRoutes);
+adminRoutes.use("/bookings", bookingsRoutes);
